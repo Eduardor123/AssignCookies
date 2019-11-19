6b4fac65b7e131f14d3c4ad3f2542d3511b3b40c
Eduardo Rocha <eroch007@ucr.edu>
2019-11-19 15:32:59 -0800

    adding shit to gitignore

---
+++ CMakeFiles/test_assigncookies.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_assigncookies.dir/test.cpp.o"
+  "test_assigncookies"
+  "test_assigncookies.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_assigncookies.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
