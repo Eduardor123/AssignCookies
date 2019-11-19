5803f798e4a4efc4e5858a7ad174a1844acc4253
Eduardo Rocha <eroch007@ucr.edu>
2019-11-19 15:18:21 -0800

    couple tests running correctly

--- test.cpp
+++ test.cpp
@@ -4,13 +4,20 @@
 #include "assign_cookies.hpp"
 
 TEST(CorrectOutput, MinCookies){
-   std::vector<int> first = {1,2,3};
-   std::vector<int> sec = {1,1};
-   Solution me;
-   int contentChildren = me.findContentChildren(first,sec);
-   EXPECT_EQ(contentChildren,1);
+    std::vector<int> first = {1,2,3};
+    std::vector<int> sec = {1,1};
+    Solution me;
+    int contentChildren = me.findContentChildren(first,sec);
+    EXPECT_EQ(contentChildren,1);
 }
 
+TEST(CorrectOutput, MaxCookies){
+    std::vector<int> first = {1,2,3};
+    std::vector<int> sec = {1,2};
+    Solution me;
+    int contentChildren = me.findContentChildren(first,sec);
+    EXPECT_EQ(contentChildren,2);
+}
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
