d767088657e57ed153da23eb9b7b0ffe3567b27c
Eduardo Rocha <eroch007@ucr.edu>
2019-11-19 21:19:23 -0800

    v1.0

--- assign_cookies.hpp
+++ assign_cookies.hpp
@@ -18,17 +18,24 @@ public:
         //assuming that they are both sorted we will try this approach
         if(s.size() == 0 || g.size() == 0)
             return 0;
+
+        //Here we do a sort on both lists
         std::sort(g.begin(),g.end());
         std::sort(s.begin(),s.end());
+
+        //our variables to keep track of data we need
         int contentChildren = 0;
         int j = 0 , i =0;
+
+        //runs until either ones length length capacity
         while(i< g.size() && j < s.size()){
-            if(g.at(i) > s.at(j)){
-                j++;
-            }else{
-               j++;
-                contentChildren++;
+            //if the condition where the cookie is less than the childs greed
+            if(g.at(i) > s.at(j))
+                j++; //go to next cookie
+            else{
+                j++; //
                 i++;
+                contentChildren++;
             }
         }
         return contentChildren;
